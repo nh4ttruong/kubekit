04907a9195e1c91becad13fee089f57bd7d35120
Nhat Truong <truongtbn243@gmail.com>
2024-06-03 14:17:06 +0700

    Update kn.sh

--- kn.sh
+++ kn.sh
@@ -33,6 +33,7 @@ list_namespaces() {
     else
         current_namespace_line_number=$(echo "$all_namespaces" | grep -n -w "$current_namespace" | cut -d':' -f1)
         if [ -n "$current_namespace_line_number" ]; then
+            echo "$all_namespaces" | sed "$current_namespace_line_number s/.*/${GREEN}&${RESET}/"
             echo "[+] Current namespace: ${GREEN}${current_namespace}${RESET}"
         else
             echo "$all_namespaces"
